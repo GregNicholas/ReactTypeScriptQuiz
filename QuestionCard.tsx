15ada7305607cc70bcbb72476452d15fb6ffee53
root <gregschoenberg@gmail.com>
2022-01-21 23:11:04 +0000

    seting up components and typescript

---
+++ QuestionCard.tsx
@@ -0,0 +1,26 @@
+import React from "react"
+
+type Props = {
+	question: string;
+	answer: string[];
+	callback: any;
+	userAnswer: string;
+	questionNum: number;
+	totalQuestions: number;
+}
+
+const QuestionCard: React.FC<Props> = ({ 
+	question, 
+	answer, 
+	callback, 
+	userAnswer, 
+	questionNum, 
+	totalQuestions
+	}) => {
+	
+	return (
+		<div>QCard</div>
+	)
+}
+
+export default QuestionCard
